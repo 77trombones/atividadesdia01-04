bcc2a66635e7a75a74154b75a77a9ded7834bf18
tavoavoavo <gustavovenanandrade@gmail.com>
2025-04-02 21:23:27 -0300

    Update exercicio4.c

--- exercicio4.c
+++ exercicio4.c
@@ -8,7 +8,7 @@ int main() {
     
     for (A = 0; A <= 1; A++) {
         for (B = 0; B <= 1; B++) {
-            printf("A: %d B: %d   %d && %d = %d\n", A, B, A, B, A && B);
+            printf("%d || %d   %d && %d = %d\n", A, B, A, B, A && B);
         }
     }
     
